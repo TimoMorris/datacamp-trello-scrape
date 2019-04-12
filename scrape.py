09515f1a07e5a4e251000d7de04f38ced2938f05
Tim Morris <tim.morris@apteco.com>
2019-04-12 19:39:32 +0100

    rename variables for familiarity and clarity; refactor for PEP-8 compliance

--- scrape.py
+++ scrape.py
@@ -1,19 +1,19 @@
-import sys
-import requests as rq
+import requests
 import pandas as pd
-from trello import TrelloClient as tc
-from bs4 import BeautifulSoup as bs
+from trello import TrelloClient
+from bs4 import BeautifulSoup
+
 
 def _get_courses():
-    page = rq.get('https://www.datacamp.com/courses/all')
-    stat = str(page.status_code)
-    print('Status: ' + stat)
-    if stat != '200':
+    page = requests.get('https://www.datacamp.com/courses/all')
+    status = str(page.status_code)
+    print('Status: ' + status)
+    if status != '200':
         return
-    soup = bs(page.content, 'html.parser')
+    soup = BeautifulSoup(page.content, 'html.parser')
     course_blocks = soup.find_all(class_='course-block')
     
-    df = pd.DataFrame([], columns=['Name', 'Type', 'Description','Link'])
+    df = pd.DataFrame([], columns=['Name', 'Type', 'Description', 'Link'])
     for course in course_blocks:
         name = str(list(course.children)[1].find(class_='course-block__title').get_text())
         desc = str(list(course.children)[1].find(class_='course-block__description').get_text())[11:].replace('\n', '')
@@ -23,79 +23,84 @@ def _get_courses():
                 'course-block__technology'))[0]).replace(
                         '<div class=\"course-block__technology course-block__technology--', '').replace(
                                 '\"></div>', '')
-                         #Okay, this is pretty bad but it 
-                         #means I don't have to import re at least
+                         # Okay, this is pretty bad but it
+                         # means I don't have to import re at least
 
         df = df.append(pd.DataFrame([[name, type_, desc, link]], columns=['Name', 'Type', 'Description', 'Link']))
-    return(df.reset_index(drop=True))
-    
+    return df.reset_index(drop=True)
+
+
 def update_progress():
     return None
 
 
 def _get_tclient(creds='api.xml'):
     hand = open(creds).read()
-    soup = bs(hand, 'lxml')
-    client = tc(
+    soup = BeautifulSoup(hand, 'lxml')
+    client = TrelloClient(
             api_key=soup.find('key').get_text(),
             api_secret=soup.find('secret').get_text(),
             token=soup.find('token').get_text())
     return client
 
-def _add_card(pl, rl, ol, row, board):
+
+def _add_card(python_list, r_list, other_list, row, board):
     type_ = row['Type']
-    l = ol
+    L = other_list
     if type_ == 'python':
-        l = pl
+        L = python_list
     elif type_ == 'r':
-        l = rl
+        L = r_list
 
     dupe = False
-    for c in board.all_cards():
-        if c.name == row['Name']:
-            #duplicate so don't add
+    for card in board.all_cards():
+        if card.name == row['Name']:
+            # duplicate so don't add
             dupe = True
-    if not(dupe):
-        l.add_card(row['Name'], row['Description'] + '\n' + row['Link'])
+    if not dupe:
+        L.add_card(row['Name'], row['Description'] + '\n' + row['Link'])
+
 
 def populate_all_courses(client=_get_tclient()):
-    cb = None
-    for b in client.list_boards():
-        if b.name == 'All DC Courses':
-            cb = b
+    dc_courses_board = None
+    for board in client.list_boards():
+        if board.name == 'All DC Courses':
+            dc_courses_board = board
 
-    lists = cb.open_lists()
-    pl = None
-    rl = None
-    ol = None
-    for l in lists:
-        if l.name == 'Python':
-            pl = l
-        elif l.name == 'R':
-            rl = l
+    lists = dc_courses_board.open_lists()
+    python_list = None
+    r_list = None
+    other_list = None
+    for L in lists:
+        if L.name == 'Python':
+            python_list = L
+        elif L.name == 'R':
+            r_list = L
         else:
-            ol = l
+            other_list = L
 
     for index, row in _get_courses().iterrows():
         print(index)
-        _add_card(pl, rl, ol, row, cb)
+        _add_card(python_list, r_list, other_list, row, dc_courses_board)
+
 
 def delete_all_cards(client=_get_tclient()):
-    cb = None
-    for b in client.list_boards():
-        if b.name == 'All DC Courses':
-            cb = b
-    for c in cb.all_cards():
-        print(c)
-        c.delete()
+    dc_courses_board = None
+    for board in client.list_boards():
+        if board.name == 'All DC Courses':
+            dc_courses_board = board
+    for card in dc_courses_board.all_cards():
+        print(card)
+        card.delete()
 
 
 def main():
-    delete_all_cards()
-    populate_all_courses()
-    #update_progress()
-    #_get_courses()
+    # delete_all_cards()
+    # populate_all_courses()
+    # update_progress()
+    # _get_courses()
     exit(0)
-    
+
+
 if __name__ == '__main__':
     main()
